9da27bf131185326b8af4d3a7d4f801efa6247aa
Mathan Kumar <mathanjeevika@gmail.com>
2021-01-21 20:36:54 +0530

    second commit

--- src/main/java/com/mathan/is/controller/PlayerController.java
+++ src/main/java/com/mathan/is/controller/PlayerController.java
@@ -3,8 +3,11 @@ package com.mathan.is.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +46,7 @@ public class PlayerController {
 	}
 
 	@GetMapping("/players/{playerId}")
-	public Object getAllPlayer(Integer playerId) {
+	public Object getAllPlayer(@PathVariable("playerId") Integer playerId) {
 		return playersRepo.findById(playerId);
 	}
 
